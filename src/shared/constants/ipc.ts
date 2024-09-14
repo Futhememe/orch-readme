c1d2c8b81ddc21c106631a5ea7bc910b992ee2ff
Futheme <kyoro_souza@hotmail.com>
2024-09-14 15:11:39 -0300

    🚧 ipc type and constants

---
+++ src/shared/constants/ipc.ts
@@ -0,0 +1,13 @@
+export const IPC = {
+  PROJECTS: {
+    FETCH_ALL: 'projects: fetch-all',
+    FETCH: 'projects: fetch',
+    IMPORT: 'projects: import',
+    CREATE: 'projects: create',
+    SAVE: 'projects: save',
+    DELETE: 'projects: delete'
+  },
+  ACTIONS: {
+    SELECT_FOLDER: 'actions: select-folder'
+  }
+}
